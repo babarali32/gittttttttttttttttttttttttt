9375cf0d981054302ed05ce79ce0aac4bbaf8ecc
Babar Ali <babaraliengineer32@gmail.com>
2024-02-24 11:32:08 +0500

    minus code added

--- src/mainplayer.java
+++ src/mainplayer.java
@@ -1,11 +1,14 @@
 
 public class mainplayer {
-
+        // player 01
     public static void main(String[] args) {
         int a=6;
         int b=4;
         //sum of two numbers
         int c=a+b;
         System.out.println("");
+        //minus of two number
+        int d=a-b;
+        System.out.println(d);
     }
 }
