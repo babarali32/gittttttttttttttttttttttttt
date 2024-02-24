b96248d804099a9c9f60bfa19824c619177d11a2
Babar Ali <babaraliengineer32@gmail.com>
2024-02-24 11:33:33 +0500

    player 2 multicode added

--- src/mainplayer.java
+++ src/mainplayer.java
@@ -1,11 +1,14 @@
 
 public class mainplayer {
-
+        // player 02
     public static void main(String[] args) {
         int a=6;
         int b=4;
         //sum of two numbers
         int c=a+b;
         System.out.println("");
+        // multiple of two number code added
+        int m=a*b;
+        System.out.println(m);
     }
 }
