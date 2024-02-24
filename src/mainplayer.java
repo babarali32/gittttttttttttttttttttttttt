e9e2a7355094c6132174c177131eb54ce46093f3
Babar Ali <babaraliengineer32@gmail.com>
2024-02-24 11:16:21 +0500

    main player code is here

---
+++ src/mainplayer.java
@@ -0,0 +1,11 @@
+
+public class mainplayer {
+
+    public static void main(String[] args) {
+        int a=6;
+        int b=4;
+        //sum of two numbers
+        int c=a+b;
+        System.out.println("");
+    }
+}
